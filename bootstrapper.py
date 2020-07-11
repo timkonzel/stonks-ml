fe8e489599d0b4ef185453209dfbb74d714bb09e
timkonzel <timkonzel@gmail.com>
2020-07-10 23:09:37 -0400

    changed strategy

--- bootstrapper.py
+++ bootstrapper.py
@@ -5,18 +5,16 @@ import numpy as np
 import datetime
 
 model = tf.keras.Sequential([
-    keras.layers.Flatten(input_shape=[6]),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Dense(1, input_shape=[1])
 ])
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(),
-    loss='sparse_categorical_crossentropy',
+    optimizer='adam',
+    loss='mean_squared_error',
     metrics=['accuracy']
 )
 
-symbols = ['AMZN', 'TSLA', 'AAPL', 'MSFT', 'DAL', 'IZEA', 'GOOGL', 'FB', 'PG', 'NFLX', 'DIS', 'NVDA', 'KO', 'PEP']
+symbols = ['AMZN']
 all_input_data = []
 all_output_data = []
 
@@ -25,14 +23,18 @@ for symbol in symbols:
     all_input_data += training_data['input']
     all_output_data += training_data['output']
 
+size = len(all_input_data)
+
 training_input = np.array(all_input_data, dtype=float)
 training_output = np.array(all_output_data, dtype=float)
-model.fit(all_input_data, all_output_data, epochs=100)
+
+model.fit(training_input, training_output, epochs=4000)
 
 for symbol in symbols:
     current_input = get_most_recent_input_data(symbol)
-    time_value = datetime.datetime.fromtimestamp(current_input[1]).strftime('%Y-%m-%d %H:%M:%S')
-    current_input_string = '[ Time: ' + time_value + ', Average Price: ' + str(round(current_input[2], 2)) + ', percent change: ' + str(round(current_input[3], 3)) + ', volume: ' + str(round(current_input[4], 2)) + ', recommendation: '+ str(round(current_input[5], 2)) + ' ]'
-    print('Tomorrows prediction for ' + symbol + ' based on input: ' + current_input_string)
-    print(model.predict([current_input])[0])
+    current_input_string = '[ last close: ' + str(all_output_data[size-1]) + ' ]'
+    print('Tomorrows prediction for ' + symbol + ': last price: '+current_input_string)
+    prediction = model.predict([size, size+9, size+29])
+    print('1 day 10 day 30 day')
+    print(prediction)
     print('\n')
--- training_assistant.py
+++ training_assistant.py
@@ -5,55 +5,21 @@ import math
 def format_training_data(symbol):
     candle_data = get_candle_data(symbol)
     recommendation_data = get_recommendation_data(symbol)[::-1]
-    size = len(candle_data) - 1;
+    size = len(candle_data);
     training_input = [None] * size;
     training_output = [None] * size;
     for i in range(size):
         input_candle = candle_data[i]
-        next_candle = candle_data[i+1]
-        training_input[i] = get_input_for_candle(input_candle, symbol, recommendation_data)
-        average_input_candle_price = (input_candle['open'] + input_candle['close'] + input_candle['high'] + input_candle['low']) / 4
-        average_next_candle_price = (next_candle['open'] + next_candle['close'] + next_candle['high'] + next_candle['low']) / 4
-        average_price_change = average_next_candle_price - average_input_candle_price
-        percent_change = (average_price_change / average_input_candle_price) * 100
-        training_output[i] = classify_percentage_change(percent_change)
+        training_input[i] = i
+        training_output[i] = input_candle['close']
     return { 'input': training_input, 'output': training_output }
-
-def classify_percentage_change(change):
-    if change < -6:
-        return 0
-    elif change < -4:
-        return 1
-    elif change < -2:
-        return 2
-    elif change < -1:
-        return 3
-    elif change <= 0:
-        return 4
-    elif change < 1:
-        return 5
-    elif change < 2:
-        return 6
-    elif change < 4:
-        return 7
-    elif change < 6:
-        return 8
-    elif change >= 6:
-        return 9
 
 def get_input_for_candle(candle, symbol, recommendation_data):
     candle_time = candle['time']
     time_key = datetime.datetime.fromtimestamp(candle_time).strftime('%Y-%m-01')
     recommendation = get_recommendation_with_key(recommendation_data, time_key)
     recommendation_score = get_recommendation_score(recommendation)
-    return [
-        get_symbol_id(symbol),
-        candle_time,
-        get_average_candle_price(candle),
-        get_candle_percent_change(candle),
-        candle['volume'],
-        recommendation_score
-    ]
+    return candle_time
 
 def get_most_recent_input_data(symbol):
     candle_data = get_candle_data(symbol)
@@ -64,12 +30,7 @@ def get_most_recent_input_data(symbol):
 
 def get_symbol_id(symbol):
     return int.from_bytes(symbol.encode(), 'little')
-    
-def get_average_candle_price(candle):
-    return (candle['open'] + candle['close'] + candle['high'] + candle['low']) / 4
 
-def get_candle_percent_change(candle):
-    return ((candle['close'] - candle['open']) / candle['open']) * 100
 
 def get_recommendation_with_key(data, time_key):
     for recommendation in data:
